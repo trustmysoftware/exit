16a3a56c13f722da6ba4ececec6dea32370dccb4
Trustable Entity <trustmysoftware@pm.me>
2023-05-04 16:20:31 +0200

    add delay to exit() for version check

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ import { checkVersion } from './check-version.mjs';
 
 checkVersion()
 
-export const exit = (value = 1) => {
-    real_exit(value);
+/**
+ * A more graceful way to exit, the delay was mainly added to allow the
+ * built-in version checker some time to check if you have an outdated version
+ * of exit-it in the event that your program is (too) quick to complete.
+ * 
+ * The value of having a nice up-to-date system cannot be overstated! (even
+ * if this is at the detriment of user experience and code beauty). 
+ * If your program actually does something useful, and takes some milliseconds
+ * to complete normally, then you can optimise your call to exit() by
+ * setting the delay_ms to zero!
+ * 
+ * @param {number} exit_code 
+ * @param {number} delay_ms 
+ */
+export const exit = async (exit_code = 1, delay_ms = 1000) => {
+    await (() => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, delay_ms)
+        })
+    })()
+    real_exit(exit_code)
 }
--- npx_entrypoint.js
+++ npx_entrypoint.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 
 import { exit } from "./index.js";
-import { checkVersion } from "./check-version.mjs";
-
-checkVersion()
 
 console.info('running exit(42)...')
 exit(42)
